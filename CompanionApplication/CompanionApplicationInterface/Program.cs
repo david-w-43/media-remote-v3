83cdd6e661eaef1136b93e221eae7ebdcc2ffc1b
David Way <56871718+david-w-43@users.noreply.github.com>
2021-08-02 20:42:07 +0100

    Added tray icon

--- CompanionApplication/CompanionApplicationInterface/Program.cs
+++ CompanionApplication/CompanionApplicationInterface/Program.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CompanionApplication;
+using CompanionApplication.Core;
 
 namespace CompanionApplication.Interface
 {
     class Program
     {
-        static RemoteManager _remoteManager;
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -19,12 +17,13 @@ namespace CompanionApplication.Interface
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            
+            Application.Run(new TrayIcon());
 
-            _remoteManager = new RemoteManager();
-            var mediaInterface = _remoteManager.GetMediaApplicationInterface("iTunes");
+            //_remoteManager = new RemoteManager();
+            //var mediaInterface = _remoteManager.GetMediaApplicationInterface("iTunes");
 
-            mediaInterface.Next();
+            //mediaInterface.Next();
         }
     }
 }
---
+++ CompanionApplication/CompanionApplicationInterface/TrayIcon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using CompanionApplication.Core;
+
+namespace CompanionApplication.Interface
+{
+    class TrayIcon : ApplicationContext
+    {
+        static RemoteManager _remoteManager;
+
+        private NotifyIcon _notifyIcon;
+
+        internal TrayIcon()
+        {
+            // Instantiate notification tray icon
+            _notifyIcon = new NotifyIcon()
+            {
+                Icon = Properties.Resources.DisplayIcon,
+                ContextMenu = new ContextMenu(new MenuItem[]
+                {
+                    new MenuItem("Test", Test),
+                    new MenuItem("-"), // Separator
+                    new MenuItem("Quit", Quit)
+                }),
+                Visible = true,
+                Text = "Media Remote Companion",
+            };
+
+            _remoteManager = new RemoteManager();
+            var mediaInterface = _remoteManager.SetMediaApplicationInterface("iTunes");
+        }
+
+        /// <summary>
+        /// To run when user elects to quit from context menu
+        /// </summary>
+        private void Quit(object sender, EventArgs e)
+        {
+            // Hide and dispose of notification tray icon
+            _notifyIcon.Visible = false;
+            _notifyIcon.Dispose();
+
+            // Close the application
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// For use during testing
+        /// </summary>
+        private void Test(object sender, EventArgs e)
+        {
+            _remoteManager.ApplicationInterface?.Next();
+        }
+    }
+}
